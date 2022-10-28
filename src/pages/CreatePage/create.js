280d8981ef47a018b81bb226d5a8c699dd67b727
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-10-28 16:44:08 +0700

    make new post form and sync with api

--- src/pages/CreatePage/create.js
+++ src/pages/CreatePage/create.js
@@ -1,29 +1,44 @@
 import Dashboard from "../dashboard";
-import './style.css'
+import './style.css';
+
+import { useState } from "react";
+
+//import { useNavigate } from "react-router-dom";
 
 export function CreatePost(){
+
+    const [title,setTitle]= useState('');
+    const [author,setAuthor]= useState('');
+    const [body,setBody]= useState('');
+  
+
    return(
     <div>
         <Dashboard/>
        <div className="create-main">
-        <form className="create-form">
+
+        <form className="create-form"  method='post' action='http://localhost:5000/posts/new'>
             <div>
-            <label name='title' className="title-label">Title</label>
-            <input type='text' id="title-input" ></input>
+            <label  className="title-label">Title</label>
+            <input type='text' id="title-input" name='title'
+           /*  value={title} onChange={(e) => setTitle(e.target.value)} */></input>
             </div>
             <div>
             <label className="author-label">Author</label>
-            <input type='text' id="author-input"></input>
+            <input type='text' id="author-input" name='author'
+            /*  value={author} onChange={(e) => setAuthor(e.target.value)} */></input>
             </div>
             <div>
             <label className="body-text">Body Text</label>
-            <textarea id="body-input"></textarea>
+            <textarea id="body-input" name='text'
+             /* value={body} onChange={(e) => setBody(e.target.value)} */></textarea>
             </div>
             <div className="btn-cont">
             <button id="save-template">Save as Template</button>
-            <button id="publish-submit">Publish</button>
+            <button id="publish-submit" type="submit">Publish</button>
             </div>
         </form>
+
        </div>
     </div>
    )
